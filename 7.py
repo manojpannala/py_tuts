84202af69c50c75004a96bb32d03cc348415cf23
manojpannala <manojpannala@outlook.com>
2021-01-24 18:05:19 +0200

    file operations

--- 7.py
+++ 7.py
@@ -1,5 +1,25 @@
 #file operations
 
 myfile = open("data.txt")
-print(myfile.read())
+content = myfile.read()
+myfile.close()
+
+
+#better way to write the same code:
+
+with open("data.txt") as myfile:
+    content = myfile.read()
+
+print(content)
+
+
+#write to the file:
+
+with open("data.txt", "a+") as myfile:
+    myfile.write("\nmango")
+    myfile.seek(0)
+    content = myfile.read()
+
+print(content)
+
 
