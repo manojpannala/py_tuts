d5a14792d68f3fbd221aabeddc8d398c0c99e0ee
manojpannala <manojpannala@outlook.com>
2021-01-25 15:38:44 +0200

    case sensitive update

--- task1/task1_english_translator.py
+++ task1/task1_english_translator.py
@@ -2,4 +2,15 @@ import json
 
 data = json.load(open("data.json"))
 
-print(data["rain"])
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please check again."
+
+
+word = input("Enter word: ")
+
+print(translate(word))
