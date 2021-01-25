1ce4f1f1b57174ce0a9fe285a8d23521014ecbef
manojpannala <manojpannala@outlook.com>
2021-01-25 15:12:19 +0200

    pandas

--- 8.py
+++ 8.py
@@ -1,10 +1,12 @@
 import time
 import os
+import pandas
 
+# using pandas
 while True:
-    if os.path.exists("data.txt"):
-        with open("data.txt") as file:
-            print(file.read())
+    if os.path.exists("temps.csv"):
+        data = pandas.read_csv("temps.csv")
+        print(data.mean()["st1"])
     else:
         print("File does not exist")
     time.sleep(10)
