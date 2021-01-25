5e42d305b505af6ad7d6915af52568266e5af2df
manojpannala <manojpannala@outlook.com>
2021-01-25 16:25:28 +0200

    final output

--- task1/task1_english_translator.py
+++ task1/task1_english_translator.py
@@ -6,17 +6,27 @@ data = json.load(open("data.json"))
 
 def translate(w):
     w = w.lower()
-    # get_close_matches(w, possibilities, n=3, ratio = 0.6)
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:  # recommended matches
-        yn = input("Did you mean %s instead? Enter Y if yes or N if no." % get_close_matches(w, data.keys())[0])
+        yn = input("Did you mean %s instead? Enter Y if yes or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please check again."
+        else:
+            return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please check again."
 
 
 word = input("Enter word: ")
 
-print(translate(word))
+# human readable output
+output = (translate(word))
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
