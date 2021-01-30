0f8f13fad72662eb27cf671d7cb683514bbe81b1
manojpannala <manojpannala@outlook.com>
2021-01-30 21:33:52 +0200

    string_challenges

---
+++ string2.py
@@ -0,0 +1,17 @@
+# Create a function named every_other_letter that takes a string named word as a parameter. 
+# The function should return a string containing every other letter in word.
+
+# Write your every_other_letter function here:
+def every_other_letter(word):
+  every_other = ""
+  for i in range(0, len(word), 2):
+    every_other += word[i]
+  return every_other
+
+# Uncomment these function calls to test your tip function:
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
+print(every_other_letter(""))
+# should print 
